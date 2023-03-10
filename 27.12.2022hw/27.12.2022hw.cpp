701e440df478108ee7459b6f1f38b4c6885db9d4
pashka <pashatett1@mail.ru>
2023-03-11 01:34:46 +0300

    Добавьте файлы проекта.

---
+++ 27.12.2022hw/27.12.2022hw.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+#include "dz11.h"
+#include "dz12.h"
+
+int main() {
+    dz12 <char> a(5);
+    a.fill();
+    a.print();
+    a + 5;
+    a.print();
+}
---
+++ 27.12.2022hw/dz12.h
@@ -0,0 +1,117 @@
+#pragma once
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+using namespace std;
+template <class T>
+class dz12{
+	T* arr;
+	int size;
+	T* copy(int newSize, int oldSize) {
+		T* newArr = new T[newSize];
+		for (int i = 0, k = 0; i < newSize; i++) {
+			if (i < oldSize) {
+				newArr[i] = arr[k];
+				k++;
+			}
+			else newArr[i] = NULL;
+		}
+		return newArr;
+	}
+public:
+	dz12(int s) : size{ s }, arr{ new T[size] } {};
+	dz12() : Array(1) {};
+	void operator+(int value) {
+		int oldSize = size;
+		size += value;
+		arr = copy(size, oldSize);
+	}
+	void operator-(int value) {
+		int oldSize = size;
+		size -= value;
+		arr = copy(size, oldSize);
+	}
+	void operator*(int value) {
+		int oldSize = size;
+		size *= value;
+		arr = copy(size, oldSize);
+	}
+	void operator/(int value) {
+		int oldSize = size;
+		size /= value;
+		arr = copy(size, oldSize);
+	}
+	T findMax() {
+		if (is_same<T, char>::value) {
+			char* max = new char[50];
+			for (int i = 1; i < size; i++) {
+				if (arr[i] > max)
+					max = arr[i];
+			}
+			return max;
+		}
+		else {
+			int max = arr[0];
+			for (int i = 1; i < size; i++) {
+				if (arr[i] > max)
+					max = arr[i];
+			}
+			return max;
+		}
+	}
+	T findMin() {
+		if (is_same<T, char>::value) {
+			char* min = new char[50];
+			for (int i = 1; i < size; i++) {
+				if (arr[i] < min)
+					min = arr[i];
+			}
+			return min;
+		}
+		else {
+			int min = arr[0];
+			for (int i = 1; i < size; i++) {
+				if (arr[i] < min)
+					min = arr[i];
+			}
+			return min;
+		}
+	}
+	void print() {
+		for (int i = 0; i < size; i++) {
+			std::cout << arr[i] << " ";
+		}
+		std::cout << std::endl;
+	}
+	void fillRand() {
+		srand(time(NULL));
+		if (is_same<T, char>::value) 
+			for (int i = 0; i < size; i++) 
+				arr[i] = rand() % 25 + 27;
+		else if (is_same<T, int>::value) 
+			for (int i = 0; i < size; i++)  
+				arr[i] = rand() % 100;
+		else if (is_same<T, float>::value || is_same<T, double>::value) 
+			for (int i = 0; i < size; i++) 
+				arr[i] = (float)rand() / RAND_MAX / 100; 
+		else if (is_same<T, long>::value) 
+			for (int i = 0; i < size; i++)
+				arr[i] = rand() % 10000000; 
+
+		else if (is_same<T, long long>::value) 
+			for (int i = 0; i < size; i++) 
+				arr[i] = rand() % 1000000000000000; 
+	}
+	void fill() {
+		for (int i = 0; i < size; i++) {
+			cout << "Enter a " << i + 1 << " element - ";
+			cin >> arr[i];
+		}
+
+	}
+	T clear() {
+		delete[] arr;
+		arr = new T[size];
+		return arr;
+	}
+};
